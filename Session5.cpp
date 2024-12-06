3de4678c8833a533a287c8ad7f372ea6bf7bfa2d
a7mdt <125561779+a7mdt@users.noreply.github.com>
2024-12-06 12:36:51 +0200

    Add files via upload

---
+++ Session5.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //for (int i = 0; i <= 10; i*=2) {
+    //    cout << i ;
+    //}
+
+    //----------------
+
+    //for (int i = 1; i <= 24; i++) {
+    //    cout << i << endl;
+    //}
+
+    //----------------
+
+    //for (int i = 1; i <= 24; i *= 2) {
+    //    cout << i << endl;
+    //}
+
+    //---------------
+
+    //int y=0;
+    //for (int i = 0; i < 5; i++) {
+    //    y += i; // y = y + i // 0 + 1 + 2 + 3 + 4
+    //}
+    //cout << y << endl;
+
+    //---------------
+
+    //int i;
+    //cin >> i;
+    //while (i>=5)
+    //{
+    //    cout << i << endl;
+    //    i--;
+    //}
+
+    //---------------
+
+    //int x; 
+    //cin >> x;
+    //do {
+    //    cout << x << endl;
+    //} while (x >= 5);
+
+    //---------------
+
+    //int choice1;
+    //do {
+    //    cout << "1. Check Balance\n2. Deposit Money\n3. Cash Withdrawal\n4. Exit" << endl;
+    //    cout << "Enter ur Choice: ";
+    //    cin >> choice1;
+    //    cout << "\n--------------------\n";
+    //} while (choice1 > 0 && choice1 < 4 );
+
+    //---------------
+
+    //int choice2;
+    //cin >> choice2;
+    //while (choice2 < 4) {
+    //    cout << "1. Check Balance\n2. Deposit Money\n3. Cash Withdrawal\n4. Exit" << endl;
+    //    cout << "Enter ur Choice: ";
+    //    cout << "\n--------------------\n";
+    //    break;
+    //}
+
+
+
+
+}
+
